d65a563c6725534d9e5c520b5fd82d84e3c54851
Maksim Kostromin <daggerok@gmail.com>
2016-12-08 23:09:56 +0200

    add colllection

--- client/app/app.js
+++ client/app/app.js
@@ -3,6 +3,25 @@ import Backbone from 'backbone';
 import $ from 'jquery';
 
 const User = Backbone.Model.extend({ urlRoot: '/api/users', });
+const Users = Backbone.Collection.extend({
+  model: User,
+  url: '/api/users',
+});
+const users = new Users();
+const user1 = users.add({ username: '1' });
+const user2 = users.create({ username: '2' });
+
+const ul = $('div#app').html('<ul></ul>');
+
+users.fetch().then(resp => {
+  ul.html(resp._embedded.users.map(user => {
+    const { id, username } = user;
+    console.log(id, username);
+    return `<li>user id: ${id}, name: ${username}</li>`;
+  }));
+});
+
+/*
 const user = new User({ username: 'Ololo' });
 
 console.log('before save', user.toJSON());
@@ -17,3 +36,4 @@ user.save()
         <pre>${JSON.stringify(data, null, 1)}</pre>
       `);
     });
+*/
